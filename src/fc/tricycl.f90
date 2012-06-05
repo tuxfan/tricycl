d6ed80d7c818fed9206347f68d9823c6cd6a52b9
Ben Bergen <bergen@lanl.gov>
2012-06-05 00:06:19 -0600

    Rearranging for Fortran interface.

---
+++ src/fc/tricycl.f90
@@ -0,0 +1,8 @@
+!------------------------------------------------------------------------------!
+!------------------------------------------------------------------------------!
+
+module tricycl
+   use, intrinsic :: ISO_C_BINDING
+   use :: tricycl_bindings
+   use :: tricycl_interface
+end module tricycl
